e0391b5ce14dc9b33478447c13dc51f10907bc77
buvankarthik <karthikbuvan.bu@gmail.com>
2025-11-26 11:35:54 +0100

    TradeOfferList

--- Classes/src/TradeOfferList.java
+++ Classes/src/TradeOfferList.java
@@ -1,3 +1,16 @@
-public class TradeOfferList
-{
+import java.util.ArrayList;
+public class TradeOfferList {
+  private final ArrayList<TradeOffer> tradeOfferList = new ArrayList<>();
+  public TradeOfferList() {
+  }
+  public ArrayList<TradeOffer> getTradeOfferList() {
+    return new ArrayList<>(tradeOfferList);
+  }
+  public void add(TradeOffer tradeOffer) {
+    tradeOfferList.add(tradeOffer);
+  }
+  public void remove(TradeOffer tradeOffer){
+    tradeOfferList.remove(tradeOffer);
+  }
 }
+
