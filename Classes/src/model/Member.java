b2da91592dbae30842cd760114336876b82a2cbb
Artem Salatskyi <78max02@gmail.com>
2025-12-02 20:21:06 +0200

    +++

--- Classes/src/model/Member.java
+++ Classes/src/model/Member.java
@@ -2,7 +2,16 @@ package model;
 
 import java.io.Serializable;
 
-public class Member implements Serializable {
+/**
+ *
+ *
+ * @author Nandor Hock
+ *
+ * @version 02.12.2025
+ */
+public class Member implements Serializable
+{
+
   private String firstName;
   private String lastName;
   private String phoneNumber;
@@ -11,7 +20,18 @@ public class Member implements Serializable {
   private boolean boost;
   private Address address;
   private int lastRecordTime;
-  public Member(String firstName, String lastName, String phoneNumber, String email, int houseNumber, String street) {
+
+  /**
+   *
+   * @param firstName
+   * @param lastName
+   * @param phoneNumber
+   * @param email
+   * @param houseNumber
+   * @param street
+   */
+  public Member(String firstName, String lastName, String phoneNumber, String email, int houseNumber, String street)
+  {
     this.firstName = firstName;
     this.lastName = lastName;
     this.phoneNumber = phoneNumber;
@@ -21,8 +41,13 @@ public class Member implements Serializable {
     boost = false;
     lastRecordTime = 0;
   }
+
+  /**
+   *
+   */
   //Default constructor for testing purposes
-  public Member(){
+  public Member()
+  {
     firstName = "Bob";
     lastName = "Green";
     phoneNumber = "+45694200";
@@ -32,92 +57,255 @@ public class Member implements Serializable {
     boost = false;
     lastRecordTime = 0;
   }
-  public void setFirstName(String firstName) {
+
+  /**
+   *
+   * @param firstName
+   */
+  public void setFirstName(String firstName)
+  {
     this.firstName = firstName;
   }
-  public String getFirstName() {
+
+  /**
+   *
+   * @return
+   */
+  public String getFirstName()
+  {
     return firstName;
   }
-  public void setLastName(String lastName) {
+
+  /**
+   *
+   * @param lastName
+   */
+  public void setLastName(String lastName)
+  {
     this.lastName = lastName;
   }
-  public String getLastName() {
+
+  /**
+   *
+   * @return
+   */
+  public String getLastName()
+  {
     return lastName;
   }
-  public void setPhoneNumber(String phoneNumber) {
+
+  /**
+   *
+   * @param phoneNumber
+   */
+  public void setPhoneNumber(String phoneNumber)
+  {
     this.phoneNumber = phoneNumber;
   }
-  public String getPhoneNumber() {
+
+  /**
+   *
+   * @return
+   */
+  public String getPhoneNumber()
+  {
     return phoneNumber;
   }
-  public void setEmail(String email) {
+
+  /**
+   *
+   * @param email
+   */
+  public void setEmail(String email)
+  {
     this.email = email;
   }
-  public String getEmail() {
+
+  /**
+   *
+   * @return
+   */
+  public String getEmail()
+  {
     return email;
   }
-  public void setAddress(Address address) {
+
+  /**
+   *
+   * @param address
+   */
+  public void setAddress(Address address)
+  {
     this.address = address;
   }
-  public Address getAddress() {
+
+  /**
+   *
+   * @return
+   */
+  public Address getAddress()
+  {
     return address;
   }
-  public void setAddressHouse(int houseNumber) {
+
+  /**
+   *
+   * @param houseNumber
+   */
+  public void setAddressHouse(int houseNumber)
+  {
     address.setHouseNumber(houseNumber);
   }
-  public int getAddressHouse() {
+
+  /**
+   *
+   * @return
+   */
+  public int getAddressHouse()
+  {
     return address.getHouseNumber();
   }
-  public void setAddressStreet(String street) {
+
+  /**
+   *
+   * @param street
+   */
+  public void setAddressStreet(String street)
+  {
     address.setStreet(street);
   }
-  public String getAddressStreet() {
+
+  /**
+   *
+   * @return
+   */
+  public String getAddressStreet()
+  {
     return address.getStreet();
   }
-  public int getLastRecordTime() {
+
+  /**
+   *
+   * @return
+   */
+  public int getLastRecordTime()
+  {
     return lastRecordTime;
   }
-  public void setLastRecordTime(int time) {
+
+  /**
+   *
+   * @param time
+   */
+  public void setLastRecordTime(int time)
+  {
     lastRecordTime = time;
   }
-  public void setBoost(boolean boost) {
+
+  /**
+   *
+   * @param boost
+   */
+  public void setBoost(boolean boost)
+  {
     this.boost = boost;
   }
-  public boolean isBoosted() {
+
+  /**
+   *
+   * @return
+   */
+  public boolean isBoosted()
+  {
     return boost;
   }
-  public void setPoints(int points) {
+
+  /**
+   *
+   * @param points
+   */
+  public void setPoints(int points)
+  {
     this.points = points;
   }
-  public int getPoints() {
+
+  /**
+   *
+   * @return
+   */
+  public int getPoints()
+  {
     return points;
   }
-  public void convertPoints(){
+
+  /**
+   *
+   */
+  public void convertPoints()
+  {
     Community.getInstance().addCommunityPoints(points);
     points = 0;
   }
-  public void timePassed(int time){
+
+  /**
+   *
+   * @param time
+   */
+  public void timePassed(int time)
+  {
     lastRecordTime +=time;
   }
-  public void addPoints(int pointAmount){
+
+  /**
+   *
+   * @param pointAmount
+   */
+  public void addPoints(int pointAmount)
+  {
     points += pointAmount;
   }
-  public void removePoints(int pointAmount){
+
+  /**
+   *
+   * @param pointAmount
+   */
+  public void removePoints(int pointAmount)
+  {
     points -= pointAmount;
   }
-  public boolean isEligibleForReset() {
+
+  /**
+   *
+   * @return
+   */
+  public boolean isEligibleForReset()
+  {
     return points >= 100;  //This value is magical
   }
-  public boolean equals(Member obj) {
+
+  /**
+   *
+   * @param obj
+   * @return
+   */
+  public boolean equals(Member obj)
+  {
     return firstName.equals(obj.getFirstName()) && lastName.equals(
         obj.getLastName()) && phoneNumber.equals(obj.getPhoneNumber())
         && email.equals(obj.getEmail()) && points == obj.getPoints()
         && boost == obj.isBoosted() && address.equals(obj.getAddress())
         && lastRecordTime == obj.getLastRecordTime();
   }
-  public String toString() {
+
+  /**
+   *
+   * @return
+   */
+  public String toString()
+  {
     String temp = "My name is %S %S, my phone number is: %s, my email is: %s, I live at %S %d, my points is: %d, time since last record: %d, am I boosted? %B";
     return String.format(temp, lastName,firstName , phoneNumber, email,
         getAddressStreet(), getAddressHouse(), lastRecordTime, points, boost);
   }
+
 }
